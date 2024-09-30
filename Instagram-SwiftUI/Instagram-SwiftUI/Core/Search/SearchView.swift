7778e13548e65bc064526f21fee27b8810637f38
MYarovoi <m.yarovoi96@gmail.com>
2024-09-30 17:04:26 +0300

    Login View

---
+++ Instagram-SwiftUI/Instagram-SwiftUI/Core/Search/SearchView.swift
@@ -0,0 +1,49 @@
+//
+//  SearchView.swift
+//  Instagram-SwiftUI
+//
+//  Created by Mykyta Yarovoi on 27.09.2024.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    @State private var searchText = ""
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0 ... 15, id: \.self) { user in
+                        HStack(spacing: 12) {
+                            Image("images (1)")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                            
+                            VStack(alignment: .leading) {
+                                Text("Freddy")
+                                    .fontWeight(.semibold)
+                                
+                                Text("American bully")
+                            }
+                            .font(.footnote)
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search...")
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    SearchView()
+}
