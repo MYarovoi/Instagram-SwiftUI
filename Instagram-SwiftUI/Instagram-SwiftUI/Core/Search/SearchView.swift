566d11b9cbc69fe5dbdda6df711354bfb26b4cfe
MYarovoi <m.yarovoi96@gmail.com>
2024-10-01 13:27:45 +0300

    User Data Model

--- Instagram-SwiftUI/Instagram-SwiftUI/Core/Search/SearchView.swift
+++ Instagram-SwiftUI/Instagram-SwiftUI/Core/Search/SearchView.swift
@@ -14,19 +14,21 @@ struct SearchView: View {
         NavigationStack {
             ScrollView {
                 LazyVStack {
-                    ForEach(0 ... 15, id: \.self) { user in
+                    ForEach(User.MOCK_USERS) { user in
                         HStack(spacing: 12) {
-                            Image("images (1)")
+                            Image(user.profileImageUrl ?? "")
                                 .resizable()
                                 .scaledToFill()
                                 .frame(width: 40, height: 40)
                                 .clipShape(Circle())
                             
                             VStack(alignment: .leading) {
-                                Text("Freddy")
+                                Text(user.username)
                                     .fontWeight(.semibold)
                                 
-                                Text("American bully")
+                                if let fullname = user.fullname {
+                                    Text(fullname)
+                                }
                             }
                             .font(.footnote)
                             
---
+++ Instagram-SwiftUI/Instagram-SwiftUI/Model/User.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  Instagram-SwiftUI
+//
+//  Created by Mykyta Yarovoi on 01.10.2024.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: String
+    var username: String
+    var profileImageUrl: String?
+    var fullname: String?
+    var bio: String?
+    let email: String
+}
+
+extension User {
+    static var MOCK_USERS: [User] = [
+        .init(id: NSUUID().uuidString, username: "Teddy", profileImageUrl: "images (1)", fullname: "Teddy Iron", bio: "woof-woof", email: "teddy@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "Viktor", profileImageUrl: "images (2)", fullname: "Viktor Jun", bio: "wooooof", email: "v.jun@gmai.com"),
+        .init(id: NSUUID().uuidString, username: "Julia", profileImageUrl: "images", fullname: "Julia Cute", bio: "Do you like me", email: "jul@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "Sem", profileImageUrl: "ProfileImage", fullname: "Sem Smith", bio: "Meyy", email: "Smith@gmail.com")
+    ]
+}
