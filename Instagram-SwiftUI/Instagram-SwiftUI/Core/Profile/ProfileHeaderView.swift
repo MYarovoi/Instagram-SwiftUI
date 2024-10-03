8d13cff271988677db025b7545ac479a711fb9ba
MYarovoi <m.yarovoi96@gmail.com>
2024-10-03 12:30:00 +0300

    Profile Action Button

--- Instagram-SwiftUI/Instagram-SwiftUI/Core/Profile/ProfileHeaderView.swift
+++ Instagram-SwiftUI/Instagram-SwiftUI/Core/Profile/ProfileHeaderView.swift
@@ -45,17 +45,23 @@ struct ProfileHeaderView: View {
             .padding(.horizontal)
             
             Button {
-                
+                if user.isCurrentUser {
+                    
+                } else {
+                    
+                }
             } label: {
-                Text("Edit Profile")
+                Text(user.isCurrentUser ? "Edit Profile" : "Follow")
                     .font(.subheadline)
                     .fontWeight(.semibold)
                     .frame(maxWidth: .infinity)
                     .frame(height: 32)
 //                        .frame(width: 360, height: 32)
-                    .foregroundColor(.black)
+                    .background(user.isCurrentUser ? .white : Color(.systemBlue))
+                    .foregroundColor(user.isCurrentUser ? .black : .white)
+                    .cornerRadius(6)
                     .overlay(RoundedRectangle(cornerRadius: 6)
-                        .stroke(Color.gray, lineWidth: 1))
+                        .stroke(user.isCurrentUser ? .gray : .clear, lineWidth: 1))
             }
             .padding(.horizontal)
             
