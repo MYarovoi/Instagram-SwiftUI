328af31cceac54b7120e5481e4acc6af551cad0c
MYarovoi <m.yarovoi96@gmail.com>
2024-10-03 14:23:59 +0300

    Update User Data

--- Instagram-SwiftUI/Instagram-SwiftUI/Core/Profile/View/EditProfileView.swift
+++ Instagram-SwiftUI/Instagram-SwiftUI/Core/Profile/View/EditProfileView.swift
@@ -10,7 +10,11 @@ import PhotosUI
 
 struct EditProfileView: View {
     @Environment(\.dismiss) var dismiss
-    @StateObject var viewModel = EditProfileViewModel()
+    @StateObject var viewModel: EditProfileViewModel
+    
+    init(user: User) {
+        self._viewModel = StateObject(wrappedValue: EditProfileViewModel(user: user))
+    }
     
     var body: some View {
         VStack {
@@ -31,7 +35,7 @@ struct EditProfileView: View {
                     Spacer()
                     
                     Button {
-                        
+                        Task { try await viewModel.updateUserDate()}
                     } label: {
                         Text("Done")
                             .font(.subheadline)
@@ -103,5 +107,5 @@ struct EditProfileRowView: View {
 }
 
 #Preview {
-    EditProfileView()
+    EditProfileView(user: User.MOCK_USERS[0])
 }
--- Instagram-SwiftUI/Instagram-SwiftUI/Core/Profile/ViewModel/EditProfileViewModel.swift
+++ Instagram-SwiftUI/Instagram-SwiftUI/Core/Profile/ViewModel/EditProfileViewModel.swift
@@ -8,9 +8,11 @@
 import Foundation
 import PhotosUI
 import SwiftUI
+import FirebaseFirestore
 
 @MainActor
 class EditProfileViewModel: ObservableObject {
+    @Published var user: User
     @Published var selectedImage: PhotosPickerItem? {
         didSet {
             Task { await loadImage(fromItem: selectedImage)}
@@ -21,11 +23,31 @@ class EditProfileViewModel: ObservableObject {
     @Published var fullname = ""
     @Published var bio = ""
     
+    init(user: User) {
+        self.user = user
+    }
+    
     func loadImage(fromItem item: PhotosPickerItem?) async {
         guard let item = item else { return }
         
         guard let data = try? await item.loadTransferable(type: Data.self) else { return }
         guard let uiImage = UIImage(data: data) else { return }
         self.profileImage = Image(uiImage: uiImage)
+    }
+    
+    func updateUserDate() async throws {
+        var data = [String: Any]()
+        
+        if !fullname.isEmpty && user.fullname != fullname {
+            data["fullname"] = fullname
+        }
+        
+        if !bio.isEmpty && user.bio != bio {
+            data["bio"] = bio
+        }
+        
+        if !data.isEmpty {
+            try await Firestore.firestore().collection("users").document(user.id).updateData(data)
+        }
     }
 }
